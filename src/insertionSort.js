d4a2337d5828b3dcd6100780321090efe46d8865
chsohn15 <chsohn15@gmail.com>
2020-09-22 10:57:53 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,16 @@
-function insertionSort(arr) {
+function insertionSort(inputArr) {
   // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  // arr is an array of unsorted integers (i.e. [3, 5, 1, 2, 6] => )
+
+    let length = inputArr.length;
+    for (let i = 1; i < length; i++) {
+        let key = inputArr[i];
+        let j = i - 1;
+        while (j >= 0 && inputArr[j] > key) {
+            inputArr[j + 1] = inputArr[j];
+            j = j - 1;
+        }
+        inputArr[j + 1] = key;
+    }
+    return inputArr;
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,24 @@
 function selectionSort(arr) {
   // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  // arr is an array of unsorted integers (i.e. [3, 5, 1, 2, 6]) => [1, 5, 3, 2, 6] => [1, 2, 3, 5, 6]
+
+  // Loop through each element
+  // find the min value from i to length - 1
+  // if the min is equal to the element then loop is over
+  // otherwise switch with min
+
+  for(i = 0; i < arr.length; i++){
+    min = i
+    for (j = i + 1; j < arr.length; j++){
+      if (arr[min] > arr[j]){
+        min = j
+      }
+    }
+    if (min !== i){
+      let temp = arr[i]
+      arr[i] = arr[min]
+      arr[min] =temp
+     }
+  }
+  return arr;
 }
